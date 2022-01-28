e40c184153b31950577d0d33ca451c90e7f1a1eb
Thomas Dimnet <thomas.dimnet@gmail.com>
2022-01-28 09:45:12 +0100

    Init the project

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by family on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
