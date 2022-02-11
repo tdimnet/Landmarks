03cbf151fab3761fceb42c46ffbd2b6cf5009f46
Thomas Dimnet <thomas.dimnet@gmail.com>
2022-02-11 11:42:30 +0100

    Check previews dynamically

--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -24,6 +24,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone 11"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+
     }
 }
